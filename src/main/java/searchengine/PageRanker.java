e8ccea325ae8749131f8ef4b7448af7d918ecd12
IndianaWeston <143821969+IndianaWeston@users.noreply.github.com>
2023-12-04 16:08:41 +0100

    Created PageRanker class and provided with methods Created methods for TF, IDF, and TF-IDF. Have made methods for the two first methods. More are still needed. Made the Pageranker interact with the Database in order to retreive the total number of Pages. Added minor changes to the constructor of the database to make the number of pages easily retreiveable. Some more testing can perhaps be necessary

--- src/main/java/searchengine/PageRanker.java
+++ src/main/java/searchengine/PageRanker.java
@@ -2,4 +2,20 @@ package searchengine;
 
 public class PageRanker {
     
+   public static double computeTF (String term, Page page){
+    double termInDoc = page.getContent().stream().filter(s -> s.equals(term)).count(); //number of times term is in document
+    double totalTerms = page.getContent().size(); //Number of terms (repetitions allowed) in document
+    return termInDoc/totalTerms; //Used double to avoid casting and avoiding int division
+    }
+
+    public static double computeIDF (Database database, String term){
+        double totalDocs = database.pagesInDataBase(); //Total number of documents in database
+        double docsWithTerm = database.matchWord(term).size(); //Total number of documents in database with searchterm
+        return Math.log(totalDocs/docsWithTerm); //Computation of IDF
+    }
+
+    public static double computeTFIDF (Page page, String term, Database database) { //computes the TF-IDF value for the given page in a given database, for the given search term
+        return (computeIDF(database, term))*computeTF(term, page);
+    }
+
 }
