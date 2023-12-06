5cc8c0cde9577f8fc1a477186412abacb8313c89
Andreas Riget Bagge <andbag@live.dk>
2023-12-06 16:21:20 +0100

    Made a separate Test-class for the Page-class Moved relevant tests from DatabaseTest-class to this class. Still seems to work as intended

--- src/test/java/searchengine/DatabaseTest.java
+++ src/test/java/searchengine/DatabaseTest.java
@@ -76,29 +76,4 @@ public class DatabaseTest {
         Set<Page> actualResults = databaseUnderTest.matchQuery(q);
         assertEquals(expectedResults, actualResults);
     }
-
-    @Test void Page_ConvertibleList_createsCorrectPageObject() {
-        try {
-            List<String> lines = Files.readAllLines(Paths.get("new_data/test-file-database1.txt"));
-            Page page = new Page(lines.subList(0, 4));
-            assertEquals(new Page("title1", "http://page1.com", lines), page);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } 
-    }
-
-    //Test to check for equality in the content-field of the pages, since they are not compared with equals-method
-    @Test void Page_ConvertibleList_createsCorrectContent() {
-        try {
-            List<String> lines = Files.readAllLines(Paths.get("new_data/test-file-database1.txt"));
-            Page page = new Page(lines.subList(0, 4));
-            Page expectedPage = new Page("title1",
-                                           "http://page1.com", 
-                                               lines.subList(2, 4));
-            assertEquals(expectedPage.getContent(),page.getContent());
-        } catch (Exception e) {
-            e.printStackTrace();
-        } 
-    }
-
 }
