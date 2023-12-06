90a51725c52510310e80e82ad77e60b88fbabe63
IndianaWeston <143821969+IndianaWeston@users.noreply.github.com>
2023-12-06 16:49:33 +0100

    Added test to QueryHandlerTest
    * Side note; should all methods in QueryHandler.java be public?

--- src/test/java/searchengine/QueryHandlerTest.java
+++ src/test/java/searchengine/QueryHandlerTest.java
@@ -1,10 +1,52 @@
 package searchengine;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.TestInstance.Lifecycle;
+
+
+import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
 
+
+
 public class QueryHandlerTest {
+
+
+    @Test void SearchTest(){
+        try {
+            QueryHandler QueryHandlerTest = new QueryHandler("new_data/test-file-database2.txt"); 
+            List <Page> search = QueryHandlerTest.search("Test");
+        } catch (Exception e) {
+            fail("fail" + e.getMessage());
+        }
+    }
+
+    @Test void TestSplitSearchString() {
+
+        try {
+            QueryHandler QueryHandlerTest = new QueryHandler("new_data/test-file-database2.txt"); 
+            Set<Set<String>> split = QueryHandlerTest.splitSearchString("Test OR Query");
+            int expectedResults = 1; 
+            assertEquals(expectedResults, split.size());
+
+        } catch (Exception e) {
+            e.getMessage(); 
+        }
+
+    }
+
+    @Test void TestSplitString(){
+        try {
+            QueryHandler QueryHandlerTest = new QueryHandler("new_data/test-file-database2.txt");
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+
 
 }
