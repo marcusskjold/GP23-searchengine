6f1c653693a0473c8ab13106dfbde4b8c7d7646a
IndianaWeston <143821969+IndianaWeston@users.noreply.github.com>
2023-12-10 16:43:14 +0100

    Added documentation to QueryMatcher class

--- src/main/java/searchengine/QueryMatcher.java
+++ src/main/java/searchengine/QueryMatcher.java
@@ -4,14 +4,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 /** Matches a query with a database.
+ * @author
+ * @version
+ * 
  */
 public class QueryMatcher {
     private static Database database;
+
+    /** This method sets the database for query matching.
+     * @param database
+     */
 
     public static void setDatabase(Database database){
         QueryMatcher.database = database;
     }
 
+    /** This method is designed to match a given query with the database and returns a set of pages that match the query.
+     * @param q
+     * @return set<Page> which is a HashSet.
+     */
     public static Set<Page> matchQuery(Query q){
         Set<Page> results = new HashSet<>();
         for (Set<String> ANDSet : q.getORSet()){
@@ -23,6 +34,11 @@ public class QueryMatcher {
         return results;
     }
 
+    /** This method matches a set of words (ANDSet) with the database and returns pages that contain all the words.
+     * @param ANDSet
+     * @return The method returns a set of Page objects that match the query.
+     */
+
     private static Set<Page> matchANDSet(Set<String> ANDSet) {
         Set<Page> result = new HashSet<>();
         boolean firstWord = true;
@@ -33,6 +49,11 @@ public class QueryMatcher {
             firstWord = false;
         } return result;
     }
+
+    /** This method matches a single word with the database and returns pages that contain the word.
+     * @param word
+     * @return a set of pages which contain the search word. 
+     */
 
     public static Set<Page> matchWord(String word) {
         Set<Page> match = database.getPages(word);
