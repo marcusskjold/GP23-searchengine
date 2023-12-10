040a30aa809d5217eaee421dc6ff1d51cb56ec49
marcusskjold <marcusskjold@gmail.com>
2023-12-10 15:34:15 +0100

    Edit and add to Query documentation.

--- src/main/java/searchengine/Query.java
+++ src/main/java/searchengine/Query.java
@@ -3,59 +3,57 @@ package searchengine;
 import java.util.Set;
 import java.util.HashSet;
 
-/**
- * The query class represents a query in a search engine.It allows the construction of complex queries using sets of strings. 
- * @author 
- * @version
+/** The query class represents a query in a search engine.
+ * It allows the construction of complex queries using sets of strings.
  */
-
 public class Query {
 
-    /**
-     * The field private Set<Set<String>> orSet represents a set of OR conditions for the search query. 
-     * The field private String URLFilter stores a filter for URLs. 
+    /** 
+     * orSet represents a set of OR conditions for the search query. 
+     * URLFilter stores a filter for URLs.
+     * URLFilter is currently not used by the application.
      */
     private Set<Set<String>> orSet;
     private String URLFilter;
 
-    /**
-     * This constructor constructs a Query object through a set of sets representing OR conditions.
+    /** Constructs a new Query object through a set of sets representing OR conditions.
      * Each set within the main set represents an AND condition for search terms.
+     * <p>
+     * Query constructor makes no check for correctness, so it is the 
+     * responsibility of the those creating the Query to ensure that correct data is passed to it.
      * @param query The set of sets of strings representing the search terms. 
      */
-
     public Query(Set<Set<String>> query){
         orSet = query;
         URLFilter = "";
     }
 
-    /**
-     * Constructs a Query object with a single search term.
-     * This term is used to create a basic AND condition.
-     * @param searchTerm
+    /** Constructs a Query object with a single search term.
+     * Used for testing purposes.
+     * @param searchTerm the single word term to be searched for
      */
-    public Query(String searchTerm){
+    protected Query(String searchTerm){
         Set<String> andSet = new HashSet<String>();
         andSet.add(searchTerm);
         orSet = new HashSet<Set<String>>();
         orSet.add(andSet);
+        URLFilter = "";
     }
 
-    /**
-     * The method retrieves the OR condition(s) of the query. 
-     * @return the method returns a set of sets of Strings, which is representative of the OR condition. 
+    /** Retrieves the OR condition(s) of the query. 
+     * @return the full query expressed as an "OR" set of "AND" conditions. 
      */
     public Set<Set<String>> getORSet() { return orSet; }
 
-    /**
-     * This method retrieves the URLFilter of the query. 
+    /** Retrieves the URLFilter of the query. 
+     * Currently unused by the application.
      * @return the method returns the URL filter String. 
      */
 
     public String getURLFilter() { return URLFilter; }
 
-    /**
-     * This method sets a filter for the URLs to be included in the search results.
+    /** This method sets a filter for the URLs to be included in the search results.
+     * Currently unused by the application.
      * @param URLsegment The string to filter URLs.
      */
 
